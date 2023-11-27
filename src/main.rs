35b7d81c9d2fcc8a2e7597a0e748a96a20305029
noscripter <zheng.xiao@urbanic.com>
2023-11-27 11:35:16 +0800

    the Collatz conjecture

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hacked by NoScripter!");
+// This is the code to solve the `Collatz conjecture` problem in Math.
+fn main() { // fn => function; main => entry point of the whole program
+    let mut x: i32 = 27; // Mutable variable binding
+    print!("{x}");      // println! is a macro
+    while x != 1 { // NOTE: while block in rust doesn't need parentheses around
+        if x % 2 == 0 { // NOTE: if block in rust doesn't need parenthese either
+            x = x / 2;
+        } else {
+            x = 3 * x + 1;
+        }
+        print!(" => {x}");
+    }
+    println!();
 }
