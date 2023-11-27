7da5112c9cb84028b4e688d10db037ce94138935
noscripter <zheng.xiao@urbanic.com>
2023-11-27 11:36:30 +0800

    notes about the mut keyword

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // This is the code to solve the `Collatz conjecture` problem in Math.
 fn main() { // fn => function; main => entry point of the whole program
+    // mut => mutable, important here, otherwise error would be thrown in compile time
     let mut x: i32 = 27; // Mutable variable binding
     print!("{x}");      // println! is a macro
     while x != 1 { // NOTE: while block in rust doesn't need parentheses around
